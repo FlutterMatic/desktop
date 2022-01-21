f21c4a688e27b06e08c7967b221aa53398bb0598
Ziyad Farhan <56755783+ZiyadF296@users.noreply.github.com>
2022-01-21 23:15:07 +0300

    Sort imports before create build with script automations.

--- scripts/bin/src/build_utils/build.dart
+++ scripts/bin/src/build_utils/build.dart
@@ -11,6 +11,7 @@ Future<void> buildWithMode(BuildMode mode, String target) async {
   try {
     String _cmd = 'flutter build $target --${mode.name}';
 
+    await shell.cd(fluttermaticDesktopPath).run('dart run import_sorter:main');
     await shell.cd(fluttermaticDesktopPath).run(_cmd);
   } catch (_) {
     print(errorPen('Failed to build with mode $mode on $target'));
