b22a22f5f59a06a821b7bccb8f9a428b703f3e17
Ziyad Farhan <56755783+ZiyadF296@users.noreply.github.com>
2022-01-22 13:13:47 +0300

    Bug fixes with viewing changelog in about dialog.

--- lib/components/dialog_templates/about/sections/changelog.dart
+++ lib/components/dialog_templates/about/sections/changelog.dart
@@ -18,11 +18,73 @@ class ChangelogAboutSection extends StatefulWidget {
 }
 
 class _ChangelogAboutSectionState extends State<ChangelogAboutSection> {
-  List<String>? _data;
+  bool _loading = true;
+  final List<String> _data = <String>[];
 
   Future<void> _loadData() async {
-    String data = await rootBundle.loadString('changelog.md');
-    setState(() => _data = data.split('<!-- Old Release -->'));
+    String data = await rootBundle.loadString('CHANGELOG.md');
+
+    List<String> _releases = <String>[];
+    String _currentRelease = '';
+
+    bool _isReleaseLine(String line) {
+      line.trim();
+
+      if (!line.startsWith('### v')) {
+        return false;
+      }
+
+      // Split the line into parts.
+      List<String> parts = line.split(' ');
+
+      // Has to be four parts.
+      if (parts.length != 4) {
+        return false;
+      }
+
+      // See if the first part is a version number.
+      List<String> _versionParts = parts[1].split('.');
+
+      for (String part in _versionParts) {
+        if (part.startsWith('v')) {
+          part = part.substring(1);
+        }
+
+        if (part.isEmpty) {
+          return false;
+        }
+
+        if (int.tryParse(part) == null) {
+          return false;
+        }
+      }
+
+      // Make sure that the last part is a release type.
+      List<String> _releaseTypes = <String>['beta', 'alpha', 'stable'];
+
+      if (!_releaseTypes.contains(parts[3].toLowerCase())) {
+        return false;
+      }
+
+      return true;
+    }
+
+    data.split('\n').forEach((String line) {
+      // Splits each release chunks into a list of lines.
+      if (_isReleaseLine(line)) {
+        _releases.add(_currentRelease);
+        _currentRelease = line;
+      } else {
+        _currentRelease += line;
+      }
+    });
+
+    _releases.add(_currentRelease);
+
+    setState(() {
+      _loading = false;
+      _data.addAll(_releases);
+    });
   }
 
   @override
@@ -33,68 +95,61 @@ class _ChangelogAboutSectionState extends State<ChangelogAboutSection> {
 
   @override
   Widget build(BuildContext context) {
-    if (_data == null) {
+    if (_loading) {
       return const Center(child: Spinner());
     } else {
       return ListView.builder(
         shrinkWrap: true,
-        itemCount: _data?.length ?? 1,
+        itemCount: _data.length,
         itemBuilder: (BuildContext context, int index) {
-          if (_data?.length == 1) {
-            return informationWidget(
-              'Couldn\'t find changelogs for this app. Try checking the changelog on GitHub.',
-              type: InformationType.error,
-            );
-          } else {
-            return Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: RoundContainer(
-                color: Colors.blueGrey.withOpacity(0.2),
-                child: MarkdownBody(
-                  data: _data![index],
-                  selectable: true,
-                  styleSheetTheme: MarkdownStyleSheetBaseTheme.platform,
-                  extensionSet: md.ExtensionSet(
-                    md.ExtensionSet.gitHubFlavored.blockSyntaxes,
-                    <md.InlineSyntax>[
-                      md.EmojiSyntax(),
-                      ...md.ExtensionSet.gitHubFlavored.inlineSyntaxes,
-                    ],
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: RoundContainer(
+              color: Colors.blueGrey.withOpacity(0.2),
+              child: MarkdownBody(
+                data: _data[index],
+                selectable: true,
+                styleSheetTheme: MarkdownStyleSheetBaseTheme.platform,
+                extensionSet: md.ExtensionSet(
+                  md.ExtensionSet.gitHubFlavored.blockSyntaxes,
+                  <md.InlineSyntax>[
+                    md.EmojiSyntax(),
+                    ...md.ExtensionSet.gitHubFlavored.inlineSyntaxes,
+                  ],
+                ),
+                styleSheet: MarkdownStyleSheet(
+                  p: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w400,
+                    color: Theme.of(context).isDarkTheme
+                        ? Colors.grey[100]
+                        : Colors.grey[900],
                   ),
-                  styleSheet: MarkdownStyleSheet(
-                    p: TextStyle(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w400,
-                      color: Theme.of(context).isDarkTheme
-                          ? Colors.grey[100]
-                          : Colors.grey[900],
-                    ),
-                    h1: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).isDarkTheme
-                          ? Colors.white
-                          : Colors.black,
-                    ),
-                    h2: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).isDarkTheme
-                          ? Colors.white
-                          : Colors.black,
-                    ),
-                    h3: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w600,
-                      color: Theme.of(context).isDarkTheme
-                          ? Colors.white
-                          : Colors.black,
-                    ),
+                  h1: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).isDarkTheme
+                        ? Colors.white
+                        : Colors.black,
+                  ),
+                  h2: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).isDarkTheme
+                        ? Colors.white
+                        : Colors.black,
+                  ),
+                  h3: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                    color: Theme.of(context).isDarkTheme
+                        ? Colors.white
+                        : Colors.black,
                   ),
                 ),
               ),
-            );
-          }
+            ),
+          );
         },
       );
     }
